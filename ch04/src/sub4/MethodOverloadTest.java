1a988030cfa16e97d3b40257ad28e73761f55f3f
dream1910 <dream1910@naver.com>
2025-07-11 11:35:25 +0900

    수정 완료

--- ch04/src/sub4/MethodOverloadTest.java
+++ ch04/src/sub4/MethodOverloadTest.java
@@ -11,13 +11,13 @@ public class MethodOverloadTest {
 		int r2 = add(2, 3);
 		double r3 = add(1.1, 3.1);
 		String r4 = add("Hello", "Java");
-		
+		String r5 = add("number_", 1);
 		
 		System.out.println("r1 : " + r1);
 		System.out.println("r2 : " + r2);
 		System.out.println("r3 : " + r3);
 		System.out.println("r4 : " + r4);
-		
+		System.out.println("r4 : " + r5);
 			}
 		
 	// 오버로드 메서드 정의
@@ -28,5 +28,9 @@ public class MethodOverloadTest {
 	public static double add(double a, double b) {return a + b;}
 	
 	public static String add(String a, String b) {return a + b;
+	}
+	public static String add(String a, int b) {
+		
+		return a + b;
 	}
 	}
